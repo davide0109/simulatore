b6aeef727e59452480ff24346bf7137a388992f1
davide0109 <d.russo@contrader.it>
2024-11-18 12:36:54 +0100

    Update version: Gestione Profili Spring attiva e rimozione di risorse JNDI.

--- mutuoCard/src/main/java/agricole/simulatore/mutuoCard/controller/TestController.java
+++ mutuoCard/src/main/java/agricole/simulatore/mutuoCard/controller/TestController.java
@@ -1,13 +1,9 @@
 package agricole.simulatore.mutuoCard.controller;
 
-import agricole.simulatore.mutuoCard.utils.ApplicationConstant;
-
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,17 +11,4 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     private static final Logger log = LoggerFactory.getLogger(TestController.class);
-
-    @Autowired
-    ApplicationConstant applicationConstant;
-
-    @GetMapping("/test")
-    public String test() {
-        return applicationConstant.getCK_KEY() + applicationConstant.getCS_KEY() + applicationConstant.getWSO2_API_AUTH() + applicationConstant.getWSO2_API_OUTPUT() + applicationConstant.getMk();
-    }
-
-    @GetMapping("/test_1")
-    public String test1() {
-        return applicationConstant.getKeyPropertiesString() + " " + applicationConstant.getKeyProperties(applicationConstant.getCK_KEY()) + " " + applicationConstant.getKeyProperties(applicationConstant.getCS_KEY());
-    }
 }
